93c959cb25b8054f7fd0535048d5235f126dbabd
Sally Chen <jc11618@nyu.edu>
2023-09-24 23:23:03 -0400

    Kinematic object 'slider' code completed

--- Assets/ScriptFolder/Slider.cs
+++ Assets/ScriptFolder/Slider.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Slider : MonoBehaviour
 {
+    public float constantSpeed = 100f;
     public Rigidbody2D slider;
-    public Vector2 leftForce = new Vector2(-10f,0f);
+    new Vector2 move = new Vector2(-1f,0f);
+
+    void Start()
+    {
+        slider = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        
+    }
 
     void FixedUpdate()
     {
-        slider.AddForce(leftForce,ForceMode2D.Impulse);
+        slider.velocity = move * constantSpeed * Time.deltaTime;
+
     }
+
 }
