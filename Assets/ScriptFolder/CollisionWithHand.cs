99e258b0395ac49a75034c89b0398727d8ed823a
Sally Chen <jc11618@nyu.edu>
2023-09-26 11:11:18 -0400

    Finalization: Visual details, camera adjustments, physics adjustments, build and run

--- Assets/ScriptFolder/CollisionWithHand.cs
+++ Assets/ScriptFolder/CollisionWithHand.cs
@@ -1,15 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class CollisionWithHand : MonoBehaviour
 {
     public Rigidbody2D ball;
-    float forceUpward = 50f;
+    public bool dead = false;
+    float forceUpward = 5f;
+    public TMP_Text SpeechUI;
+    public string winText = "YOU WIN!!";
+    public string loseText = "YOU LOSE! PRESS SPACE TO RESTART";
+    public GameObject textObject;
 
     void Start()
     {
         ball = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (dead)
+        {
+            textObject.gameObject.SetActive(true);
+            SpeechUI.text = loseText;
+        }
+
+        if(Input.GetKey(KeyCode.Space) && dead)
+        {
+            dead = false;
+            textObject.gameObject.SetActive(false);
+
+            Scene scene = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(scene.name);
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -23,5 +48,21 @@ public class CollisionWithHand : MonoBehaviour
             //speechUI.text = eatAppleDialogue;
             //InvokeRepeating("HidePanel", 0.8f, 0.0f);
         }
+
+        if(collision.gameObject.name == "stage")
+        {
+            dead = true;
+        }
     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "winingCollider")
+        {
+            textObject.gameObject.SetActive(true);
+            SpeechUI.text = winText;
+        }
+
+    }
+
 }
